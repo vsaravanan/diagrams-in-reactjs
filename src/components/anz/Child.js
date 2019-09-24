2008dcdee07fc3cbb4347e2619e3557647e3b7af
V Saravanan <saravanan.resume@gmail.com>
2019-09-24 17:41:12 +0800

    refactoring 1

--- src/components/anz/Child.js
+++ src/components/anz/Child.js
@@ -18,72 +18,67 @@ class Child extends Component {
     console.log(' clicked : ' + childname)
     var arrshow = this.state.arrshow
     arrshow[childname] = !arrshow[childname]
-    this.setState({ arrshow, clicked: childname })
-    // var list = this.props.list
-    // if (childname === 'Handle') {
-    //   debugger
-    // }
-    // list &&
-    //   list.map(v => {
-    //     console.log('list from click ' + v)
-    //     return null
-    //   })
+    this.setState({ arrshow })
+    // this.setState({ clicked: childname })
   }
 
   render() {
     const children = this.props.children
-    const clicked = this.state.clicked
-
-    // const list = children.filter(f => f.name === clicked)
-    const parentcount = this.props.parentcount
-    // console.log('parentcount ' + parentcount)
     const arrshow = this.state.arrshow
     const shownextline = this.props.show
+    const saveClick = this.onClick
+    const grands = []
+    const leaves = []
 
-    const dummy = (
-      <div className='item '>
-        <p>Facility 1</p>
-        <p>Facility 2</p>
-        <p>Facility 3</p>
-        <p>Security 1</p>
-      </div>
-    )
+    const printChildren = value => {
+      // second level child
+      const myname = value.name
+      const line2child = (
+        <div className={'item '} key={myname} onClick={e => saveClick(myname, e)}>
+          {myname}
+        </div>
+      )
+      line2.push(line2child)
 
-    // const filler = (
-    //   <div className='fillergroup '>
-    //     <div className='filler '></div>
-    //   </div>
-    // )
+      const grandchild = value.children
 
-    // if (clicked) {
-    //   let clonearr = children.slice(0)
-    //   let findlist = clonearr.pop()
-    //   var list = findlist && findlist.list
-    // }
-    var list
-    // if (clicked === 'Handle') {
-    //   debugger
-    //   // return 'Welcome'
-    // }
-    if (list && list.length > 0) {
-      // const leaf = []
-      // const mylist = list
-      // if (mylist && mylist.length > 0) {
-      //   mylist.map((v, i) => leaf.push(<p key={'p-' + i}>{v}</p>))
-      //   var leafpage = (
-      //     <div className={'myrow '} key={'leafrow'}>
-      //       <div className={'item '} key={'leafitem'}>
-      //         {leaf}
-      //       </div>
-      //     </div>
-      //   )
-      //   return leafpage
-      // }
-    } else if (children.length > 0) {
+      if (grandchild && grandchild.length > 0) {
+        // sign
+        const sign = arrshow[myname] ? '-' : '+'
+        if (sign) {
+          const signbox = (
+            <span className={'item-relative '} key={myname + '-sign'}>
+              {sign}
+            </span>
+          )
+          line2.push(signbox)
+        }
+
+        // grandchildren
+
+        const mygrand = (
+          <Child
+            children={grandchild}
+            key={myname + '-child'}
+            // parentcount={children.length}
+            show={arrshow[myname]}
+          ></Child>
+        )
+        grands.push(mygrand)
+      }
+
+      const mynewlist = value.list
+      if (mynewlist) {
+        // grands.push(dummy)
+
+        const my4th = <Line4 key={myname + '-4th'} list={mynewlist} show={arrshow[myname]}></Line4>
+
+        leaves.push(my4th)
+      }
+    }
+
+    if (children.length > 0) {
       var line2 = []
-      const grands = []
-      const leaves = []
-      const saveClick = this.onClick
 
       // it is to hide but fill the space
       if (!shownextline) {
@@ -99,98 +94,17 @@ class Child extends Component {
         return emptygrands
       }
 
-      function printChildren(value) {
-        // second level child
-        const myname = value.name
-        const line2child = (
-          <div className={'item '} key={myname} onClick={e => saveClick(myname, e)}>
-            {myname}
-          </div>
-        )
-        line2.push(line2child)
-
-        const grandchild = value.children
-
-        if (grandchild && grandchild.length > 0) {
-          // sign
-          const sign = arrshow[myname] ? '-' : '+'
-          if (sign) {
-            const signbox = (
-              <span className={'item-relative '} key={myname + '-sign'}>
-                {sign}
-              </span>
-            )
-            line2.push(signbox)
-          }
-
-          // grandchildren
-
-          // var list
-
-          const listfiltered = children.filter(f => f.name === myname)
-          if (listfiltered) {
-            var list = listfiltered && listfiltered.list
-            // var clonearr = grandchild.slice(0)
-            // var findlist = clonearr.pop()
-            // var list = findlist && findlist.list
-          }
-
-          const mygrand = (
-            <Child
-              children={grandchild}
-              key={myname + '-child'}
-              parentcount={children.length}
-              list={list}
-              show={arrshow[myname]}
-            ></Child>
-          )
-          grands.push(mygrand)
-
-          // if (list) {
-          //   debugger
-          //   const my4th = (
-          //     <Line4
-          //       children={grandchild}
-          //       key={myname + '-4th'}
-          //       parentcount={children.length}
-          //       list={list}
-          //       show={true}
-          //     ></Line4>
-          //   )
-
-          //   grands.push(my4th)
-          // }
-        } else {
-          const mynewlist = value.list
-          if (mynewlist) {
-            // grands.push(dummy)
-
-            const my4th = (
-              <Line4 key={myname + '-4th'} list={mynewlist} show={arrshow[myname]}></Line4>
-            )
-
-            leaves.push(my4th)
-          }
-        }
-      }
-
-      children.forEach(printChildren)
+      children.filter(f => f.children).forEach(printChildren)
+      children.filter(f => f.list).forEach(printChildren)
 
       if (grands.length > 0) {
-        // grands.push(dummy)
-        // var line3 = <div className={'mysamerow '}>{grands}</div>
         var line3 = grands
-      } else if (leaves.length > 0) {
+      }
+      if (leaves.length > 0) {
         var line4 = <div className={'myrow '}>{leaves}</div>
       }
     }
-    // if (leafpage) {
-    //   return (
-    //     <Fragment>
-    //       <div className={'myrow '}>{leafpage}</div>
-    //     </Fragment>
-    //   )
-    // } else {
+
     return (
       <Fragment>
         <div className={'myrow '}>{line2}</div>
@@ -198,7 +112,6 @@ class Child extends Component {
         {line4}
       </Fragment>
     )
-    // }
   }
 }
 
--- src/components/anz/Line4.js
+++ src/components/anz/Line4.js
@@ -1,34 +1,16 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 
 import './anz.css'
 
 class Line4 extends Component {
-  // state = {
-  //   arrshow: {},
-  // }
-
-  componentDidMount() {
-    // var children = this.props.children
-    // var arrshow = this.state.arrshow
-    // children.map(v => (arrshow[v.name] = false))
-    // this.setState({ arrshow })
-  }
-
-  // onClick = (childname, e) => {
-  //   console.log(' childname : ' + childname)
-  //   var arrshow = this.state.arrshow
-  //   arrshow[childname] = !arrshow[childname]
-  //   this.setState({ arrshow, clicked: childname })
-  // }
   render() {
     const list = this.props.list
-    // const children = this.props.children
     const divshow = this.props.show
 
     const line4 = []
 
     function printChildren(myitem) {
-      // second level child
+      // 4th level child
       const line4item = (
         <div className={'item '} key={'line4-' + myitem}>
           {myitem}
@@ -36,12 +18,6 @@ class Line4 extends Component {
       )
       line4.push(line4item)
     }
-
-    // const dummy = (
-    //   <div className='item '>
-    //     <p>Dummy 1</p>
-    //   </div>
-    // )
 
     const filler = (
       <div className='fillergroup '>
@@ -55,23 +31,6 @@ class Line4 extends Component {
     } else {
       return filler
     }
-
-    // if (children.length > 0) {
-    //   var line2 = []
-
-    //   if (!divshow) {
-    //     const line2child = []
-    //     children.map((v, i) =>
-    //       line2child.push(<div className='filler ' key={'emptyitem' + i}></div>),
-    //     )
-    //     const emptygrands = (
-    //       <div className='fillergroup ' key={'emptygroup'}>
-    //         {line2child}
-    //       </div>
-    //     )
-    //     return emptygrands
-    //   }
-    // }
   }
 }
 
