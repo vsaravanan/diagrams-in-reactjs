cd3c9023b004996da04449c6585d0c14689c276c
V Saravanan <saravanan.resume@gmail.com>
2019-09-25 11:15:58 +0800

    splitting to be done

--- src/components/anz/Child.js
+++ src/components/anz/Child.js
@@ -1,4 +1,7 @@
 import React, { Component, Fragment } from 'react'
+// import $ from 'jquery'
+import parse from 'html-react-parser'
+
 import Line4 from './Line4'
 import './anz.css'
 
@@ -19,6 +22,11 @@ class Child extends Component {
     var arrshow = this.state.arrshow
     arrshow[childname] = !arrshow[childname]
     this.setState({ arrshow })
+    // if (childname === 'Handle') {
+    //   debugger
+    //   // $('em').html('</em></div><div class="container"><em>')
+    // }
+
     // this.setState({ clicked: childname })
   }
 
@@ -47,6 +55,7 @@ class Child extends Component {
       for (var j = 0; j < i; j++) {
         line2filler.push(<div className='filler ' key={'emptyitem-' + j}></div>)
       }
+      /* <span>lvlenline{this.props.level + 1} </span> */
 
       const encloseline2 = (
         <div className='fillergroup ' key={'emptygroup'}>
@@ -72,19 +81,46 @@ class Child extends Component {
       line2childs && line2.push(line2childs)
 
       // sign
-      const mysign = addsign(myname)
-      mysign && line2.push(mysign)
+      // const mysign = addsign(myname)
+      // mysign && line2.push(mysign)
     }
 
+    var grandchildindex = 0
     const addGrandChild = mydata => {
       // second level child
       const myname = mydata.name
       const grandchild = mydata.children
 
       // grandchildren
+      // {parse(`
+      // <li>Item 1</li>
+      // <li>Item 2</li>
+      // `)}
+      // <div class='break'></div>
+      // </div><div class="container">
+
+      // const tmp1 = parse(`      <li>Item 1</li>      `)
+      // const tmp1 = parse(`      <\dddddddddd>      `)
+
+      // {parse('</div><div class="container">')}
+
+      if (grandchildindex++ > 1) {
+      }
+
+      // const htmlFromCMS = `<li>Item 1</li>`
+      // const htmlFromCMS2 = '</span</div><div class="container"><span>'
+      // <span dangerouslySetInnerHTML={{ __html: htmlFromCMS2 }} />
 
       const mygrand = (
-        <Child children={grandchild} key={myname + '-child'} show={arrshow[myname]}></Child>
+        <Fragment>
+          <span>lvlgrand{this.props.level + 1}</span>
+          <Child
+            children={grandchild}
+            key={myname + '-child'}
+            show={arrshow[myname]}
+            level={this.props.level + 1}
+          ></Child>
+        </Fragment>
       )
       grands.push(mygrand)
     }
@@ -94,7 +130,14 @@ class Child extends Component {
       const myname = mydata.name
       const mynewlist = mydata.list
       if (mynewlist) {
-        const my4th = <Line4 key={myname + '-4th'} list={mynewlist} show={arrshow[myname]}></Line4>
+        const my4th = (
+          <Line4
+            key={myname + '-4th'}
+            list={mynewlist}
+            show={arrshow[myname]}
+            level={this.props.level + 1}
+          ></Line4>
+        )
 
         leaves.push(my4th)
       }
@@ -118,6 +161,7 @@ class Child extends Component {
 
       children.filter(f => f.list).forEach(addLine4)
       const itemfound = Object.values(arrshow).includes(true)
+      /* <span>lvlline{this.props.level + 1}</span>  */
 
       if (leaves.length > 0) {
         var line4
@@ -127,13 +171,39 @@ class Child extends Component {
           line4 = leaves
         }
       }
+
+      var lastline
+      if (this.props.level === 2) {
+        lastline = (
+          <Fragment>
+            <div className='mysamerow'>
+              <div className='container'>
+                {line3}
+                {line4}
+              </div>
+            </div>
+          </Fragment>
+        )
+      } else if (this.props.level === 3) {
+        lastline = (
+          <Fragment>
+            {line3}
+            {line4}
+          </Fragment>
+        )
+      } else {
+        lastline = (
+          <Fragment>
+            {line3} {line4}
+          </Fragment>
+        )
+      }
     }
 
     return (
       <Fragment>
         <div className={'myrow '}>{line2}</div>
-        {line3}
-        {line4}
+        {lastline}
       </Fragment>
     )
   }
--- src/components/anz/data.js
+++ src/components/anz/data.js
@@ -9,8 +9,17 @@ var data = {
           list: ['Facility 1', 'Facility 2'],
         },
         {
+          name: 'Another',
+          list: ['Facility 8'],
+        },
+      ],
+    },
+    {
+      name: 'Shade',
+      children: [
+        {
           name: 'Rain',
-          list: ['Facility 6'],
+          list: ['Facility 6', 'Security 3'],
         },
       ],
     },
